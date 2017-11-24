b15c3bbcd8aec4595256bde3fcbdd5efa243bfef
R1163213 <Ramprit.Sahani@ext.saint-gobain.com>
2017-11-24 20:03:09 +0530

    Department

--- src/app/department/department.module.ts
+++ src/app/department/department.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DepartmentComponent } from './department.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { DepartmentService } from './department.service';
+import { ApiUrlService } from '../shared/apiUrl.service';
+import { DataTableModule, SharedModule, PanelModule } from 'primeng/primeng';
 @NgModule({
   imports: [
-    CommonModule, HttpClientModule, RouterModule.forChild([
+    CommonModule, HttpClientModule, DataTableModule, PanelModule, RouterModule.forChild([
       { path: '', pathMatch: 'full', redirectTo: 'department' },
       { path: 'department', component: DepartmentComponent }
     ])
   ],
   declarations: [DepartmentComponent],
-  providers: []
+  providers: [DepartmentService, ApiUrlService]
 })
 export class DepartmentModule { }
